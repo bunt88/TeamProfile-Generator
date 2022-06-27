ce21b31a2efb7909ddedb30195c2f66dd4472498
unknown <bunt88@gmail.com>
2022-06-27 16:29:38 -0600

    added html template and card creation

---
+++ index.js
@@ -0,0 +1,20 @@
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Create questions for user input
+const questions = () => {
+    return inquirer.prompt([]);
+};
+
+//function to write HTML file
+
+function writeToFile(data) {
+    return ``
+}
+
+//create function to initialize app
+function init() { };
+
+//Function call to initialize app
+init();
