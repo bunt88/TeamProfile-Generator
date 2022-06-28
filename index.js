7fd4879abd96c7bcab937885577ec0e4b0240ab6
unknown <bunt88@gmail.com>
2022-06-28 14:09:51 -0600

    Added User Input Prompts

--- index.js
+++ index.js
@@ -2,10 +2,96 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// Create questions for user input
-const questions = () => {
-    return inquirer.prompt([]);
+
+const menu = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'teamMember',
+            choices: ['Manager', 'Engineer', 'Intern'],
+            message: 'Would you like to add a new Team Member?'
+        },
+    ]);
 };
+
+// Managers questions for user input
+const manager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Name',
+            message: 'Enter Managers Name'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter Employee id'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter Email'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter Github Username'
+        }
+    ]);
+};
+
+// Engineers questions for user input
+const engineer = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Name',
+            message: 'Enter Engineers Name'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter Employee id'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter Email'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter Github Username'
+        }
+    ]);
+};
+
+// Intern questions for user input
+const intern = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Name',
+            message: 'Enter Interns Name'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter Employee id'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter Email'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Enter School Name'
+        }
+    ]);
+};
+
 
 //function to write HTML file
 
@@ -14,7 +100,10 @@ function writeToFile(data) {
 }
 
 //create function to initialize app
-function init() { };
+function init() {
+    menu()
+
+};
 
 //Function call to initialize app
 init();
